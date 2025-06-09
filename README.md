6cac31656821f146e456dfeae84c2e635eeba9f3
RubenThekiller13 <44271271+RubenThekiller13@users.noreply.github.com>
2025-06-09 16:23:15 +0200

    one file upload (py)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # movidas
 yokese
+wet
---
+++ mismo_item_listas.py
@@ -0,0 +1,8 @@
+list1 = ["hola", "mundo", "esto", "es", "una", "prueba"]
+list2 = ["hola", "mundo", "esto", "es", "una", "prueba"]
+
+for i in list1:
+    if i in list2: 
+        print(i)
+  
+
